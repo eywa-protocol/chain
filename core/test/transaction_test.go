00a78b5b11fabc7d8ac6d134f1379c5035436a54
Denis Glotov <denis@glotov.org>
2022-03-11 15:29:20 +0100

    TestTransaction_Serialize

--- core/test/transaction_test.go
+++ core/test/transaction_test.go
@@ -5,12 +5,12 @@ import (
 
 	"github.com/eywa-protocol/chain/common"
 	"github.com/eywa-protocol/chain/core/payload"
+	"github.com/eywa-protocol/chain/core/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTransaction_Serialize(t *testing.T) {
-	var tx payload.Payload
-	tx = &payload.InvokeCode{Code: []byte("Chain Id")}
+	tx := types.ToTransaction(&payload.InvokeCode{Code: []byte("Chain Id")})
 
 	sink := common.NewZeroCopySink(nil)
 	err := tx.Serialization(sink)
@@ -21,15 +21,13 @@ func TestTransaction_Serialize(t *testing.T) {
 	err = tx.Serialization(sink)
 	assert.NoError(t, err)
 
-	var tx1 payload.InvokeCode
-	err = tx1.Deserialization(common.NewZeroCopySource(sink.Bytes()))
+	tx1, err := types.TransactionDeserialization(common.NewZeroCopySource(sink.Bytes()))
 	assert.NoError(t, err)
-	assert.Equal(t, tx, &tx1)
+	assert.Equal(t, tx, tx1)
 }
 
 func TestEpochTransaction_Serialize(t *testing.T) {
-	var tx payload.Payload
-	tx = &payload.Epoch{Data: []byte("Chain Id")}
+	tx := types.ToTransaction(&payload.Epoch{Data: []byte("Chain Id")})
 
 	sink := common.NewZeroCopySink(nil)
 	err := tx.Serialization(sink)
@@ -43,9 +41,8 @@ func TestEpochTransaction_Serialize(t *testing.T) {
 	err = tx.Serialization(sink)
 	assert.NoError(t, err)
 
-	var tx1 payload.Epoch
-	err = tx1.Deserialization(common.NewZeroCopySource(sink.Bytes()))
+	tx1, err := types.TransactionDeserialization(common.NewZeroCopySource(sink.Bytes()))
 	assert.NoError(t, err)
-	assert.Equal(t, tx, &tx1)
+	assert.Equal(t, tx, tx1)
 	// t.Log(tx.TxType())
 }
