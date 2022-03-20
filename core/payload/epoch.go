ffe4651f7e3be165e6b259446b18fadba33a8494
Denis Glotov <denis@glotov.org>
2022-03-20 21:51:11 +0100

    Epoch payload

--- core/payload/epoch.go
+++ core/payload/epoch.go
@@ -1,13 +1,17 @@
 package payload
 
 import (
+	"encoding/binary"
 	"fmt"
 
+	"github.com/eywa-protocol/bls-crypto/bls"
 	"github.com/eywa-protocol/chain/common"
 )
 
 type Epoch struct {
-	Data []byte
+	Number         uint32          // Number of this epoch
+	EpochPublicKey bls.PublicKey   // Aggregated public key of all participants of the current epoch
+	PublicKeys     []bls.PublicKey // Public keys of all nodes
 }
 
 func (tx *Epoch) TxType() TransactionType {
@@ -15,20 +19,60 @@ func (tx *Epoch) TxType() TransactionType {
 }
 
 func (self *Epoch) Deserialization(source *common.ZeroCopySource) error {
-	code, eof := source.NextVarBytes()
+	number, eof := source.NextUint32()
 	if eof {
-		return fmt.Errorf("[InvokeCode] deserialize code error")
+		return fmt.Errorf("Epoch.Number deserialize eof")
+	}
+	epochPublicKeyRaw, eof := source.NextVarBytes()
+	if eof {
+		return fmt.Errorf("Epoch.EpochPublicKey deserialize eof")
+	}
+	self.Number = number
+
+	epochPublicKey, err := bls.UnmarshalPublicKey(epochPublicKeyRaw)
+	if err != nil {
+		return fmt.Errorf("Epoch.EpochPublicKey deserialize error %v", err)
+	}
+	self.EpochPublicKey = epochPublicKey
+
+	length, eof := source.NextUint8()
+	if eof {
+		return fmt.Errorf("Epoch.len(PublicKeys) deserialize eof")
 	}
 
-	self.Data = code
+	self.PublicKeys = make([]bls.PublicKey, 0, length)
+	for i := uint8(0); i < length; i++ {
+		publicKeyRaw, eof := source.NextVarBytes()
+		if eof {
+			return fmt.Errorf("Epoch.PublicKey deserialize eof")
+		}
+		publicKey, err := bls.UnmarshalPublicKey(publicKeyRaw)
+		if err != nil {
+			return fmt.Errorf("Epoch.PublicKey[%d] deserialize error %v", i, err)
+		}
+		self.PublicKeys = append(self.PublicKeys, publicKey)
+	}
+
 	return nil
 }
 
 func (self *Epoch) Serialization(sink *common.ZeroCopySink) error {
-	sink.WriteVarBytes(self.Data)
+	sink.WriteUint32(self.Number)
+	sink.WriteVarBytes(self.EpochPublicKey.Marshal())
+	sink.WriteUint8(uint8(len(self.PublicKeys)))
+	for _, key := range self.PublicKeys {
+		sink.WriteVarBytes(key.Marshal())
+	}
 	return nil
 }
 
 func (self *Epoch) RawData() []byte {
-	return self.Data
+	epochNumRaw := make([]byte, 4)
+	binary.BigEndian.PutUint32(epochNumRaw, self.Number)
+
+	sink := common.NewZeroCopySink(nil)
+	sink.WriteBytes(epochNumRaw)
+	sink.WriteVarBytes(self.EpochPublicKey.Marshal())
+	sink.WriteUint8(uint8(len(self.PublicKeys)))
+	return sink.Bytes()
 }
--- core/test/transaction_test.go
+++ core/test/transaction_test.go
@@ -3,6 +3,8 @@ package test
 import (
 	"testing"
 
+	"github.com/eywa-protocol/bls-crypto/bls"
+
 	"github.com/eywa-protocol/chain/common"
 	"github.com/eywa-protocol/chain/core/payload"
 	"github.com/eywa-protocol/chain/core/types"
@@ -27,10 +29,17 @@ func TestTransaction_Serialize(t *testing.T) {
 }
 
 func TestEpochTransaction_Serialize(t *testing.T) {
-	tx := types.ToTransaction(&payload.Epoch{Data: []byte("Chain Id")})
+	epoch, err := bls.ReadPublicKey("1d65becbb891b6e69951febbc4ac066343670b34d84777a077c06871beb9c07f28be8a6fa825e9d615f56f0dbcd728b46e42b4ae2a611e2ab919a1de923ae7ed0f1c89b508af036f52c2215a04e13a7a5e891d9220d3d8751dc0525b81fca3051dc2e58a167c412941bd1adeb29f5a0beb5d26e748e8ca55e508deadead1ea5e")
+	assert.NoError(t, err)
+
+	tx := types.ToTransaction(&payload.Epoch{
+		Number:         1,
+		EpochPublicKey: epoch,
+		PublicKeys:     []bls.PublicKey{},
+	})
 
 	sink := common.NewZeroCopySink(nil)
-	err := tx.Serialization(sink)
+	err = tx.Serialization(sink)
 	assert.NoError(t, err)
 
 	err = tx.Serialization(sink)
