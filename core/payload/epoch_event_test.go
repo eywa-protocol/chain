3509cf469b68a46c297a663a7bef18f3567499dc
Denis Glotov <denis@glotov.org>
2022-04-29 18:35:33 +0300

    EpochEvent updates: epoch public key and host ids (#20)

--- core/payload/epoch_event_test.go
+++ core/payload/epoch_event_test.go
@@ -12,7 +12,7 @@ func TestEpochEvent_Serialize(t *testing.T) {
 	epoch, err := bls.ReadPublicKey("1d65becbb891b6e69951febbc4ac066343670b34d84777a077c06871beb9c07f28be8a6fa825e9d615f56f0dbcd728b46e42b4ae2a611e2ab919a1de923ae7ed0f1c89b508af036f52c2215a04e13a7a5e891d9220d3d8751dc0525b81fca3051dc2e58a167c412941bd1adeb29f5a0beb5d26e748e8ca55e508deadead1ea5e")
 	assert.NoError(t, err)
 
-	event := NewEpochEvent(123, epoch, common.UINT256_EMPTY, []bls.PublicKey{epoch, epoch, epoch})
+	event := NewEpochEvent(123, common.UINT256_EMPTY, []bls.PublicKey{epoch, epoch, epoch}, []string{"one", "two", "three"})
 
 	sink := common.NewZeroCopySink(nil)
 	err = event.Serialization(sink)
@@ -23,7 +23,7 @@ func TestEpochEvent_Serialize(t *testing.T) {
 	assert.Equal(t, *event, received)
 
 	// test ToJson
-	jbExpected := `{"Number":123,"EpochPublicKey":"1d65becbb891b6e69951febbc4ac066343670b34d84777a077c06871beb9c07f28be8a6fa825e9d615f56f0dbcd728b46e42b4ae2a611e2ab919a1de923ae7ed0f1c89b508af036f52c2215a04e13a7a5e891d9220d3d8751dc0525b81fca3051dc2e58a167c412941bd1adeb29f5a0beb5d26e748e8ca55e508deadead1ea5e","SourceTx":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"PublicKeys":["1d65becbb891b6e69951febbc4ac066343670b34d84777a077c06871beb9c07f28be8a6fa825e9d615f56f0dbcd728b46e42b4ae2a611e2ab919a1de923ae7ed0f1c89b508af036f52c2215a04e13a7a5e891d9220d3d8751dc0525b81fca3051dc2e58a167c412941bd1adeb29f5a0beb5d26e748e8ca55e508deadead1ea5e","1d65becbb891b6e69951febbc4ac066343670b34d84777a077c06871beb9c07f28be8a6fa825e9d615f56f0dbcd728b46e42b4ae2a611e2ab919a1de923ae7ed0f1c89b508af036f52c2215a04e13a7a5e891d9220d3d8751dc0525b81fca3051dc2e58a167c412941bd1adeb29f5a0beb5d26e748e8ca55e508deadead1ea5e","1d65becbb891b6e69951febbc4ac066343670b34d84777a077c06871beb9c07f28be8a6fa825e9d615f56f0dbcd728b46e42b4ae2a611e2ab919a1de923ae7ed0f1c89b508af036f52c2215a04e13a7a5e891d9220d3d8751dc0525b81fca3051dc2e58a167c412941bd1adeb29f5a0beb5d26e748e8ca55e508deadead1ea5e"]}`
+	jbExpected := `{"Number":123,"EpochPublicKey":"110eb22c46a82e9c3be63df6a061537f3de17d84a66fe5a491a5aced21ef0bc101a9b68af82033d3e9cc8ae964dd6ae998926dee2df8a9b323891afdc76b6956005ee00604c14856945452b6c2d055535cf3a3325ef43b44f2bb6d7e497543f1291853b24e4ebf74cfce2087b2594e54503c88d824e68e838ff85b6b8a00ded5","SourceTx":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"PublicKeys":["1d65becbb891b6e69951febbc4ac066343670b34d84777a077c06871beb9c07f28be8a6fa825e9d615f56f0dbcd728b46e42b4ae2a611e2ab919a1de923ae7ed0f1c89b508af036f52c2215a04e13a7a5e891d9220d3d8751dc0525b81fca3051dc2e58a167c412941bd1adeb29f5a0beb5d26e748e8ca55e508deadead1ea5e","1d65becbb891b6e69951febbc4ac066343670b34d84777a077c06871beb9c07f28be8a6fa825e9d615f56f0dbcd728b46e42b4ae2a611e2ab919a1de923ae7ed0f1c89b508af036f52c2215a04e13a7a5e891d9220d3d8751dc0525b81fca3051dc2e58a167c412941bd1adeb29f5a0beb5d26e748e8ca55e508deadead1ea5e","1d65becbb891b6e69951febbc4ac066343670b34d84777a077c06871beb9c07f28be8a6fa825e9d615f56f0dbcd728b46e42b4ae2a611e2ab919a1de923ae7ed0f1c89b508af036f52c2215a04e13a7a5e891d9220d3d8751dc0525b81fca3051dc2e58a167c412941bd1adeb29f5a0beb5d26e748e8ca55e508deadead1ea5e"],"HostIds":["one","two","three"]}`
 	jb, err := received.ToJson()
 	assert.NoError(t, err)
 	assert.Equal(t, jbExpected, string(jb))
